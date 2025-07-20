6efc996cc6384079a11abc9488360c6e828d16c2
Oleg <aleynikov.oleg@gmail.com>
2025-07-20 11:57:13 +0300

    SampleController

--- SampleTracking/Controllers/HomeController.cs
+++ SampleTracking/Controllers/HomeController.cs
@@ -1,35 +1,11 @@
-using MessageBroker.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using SampleTracking.Models;
 
 namespace SampleTracking.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ISampleRepository _sampleRepository;
-    private readonly IProducer _producer;
-
-    public HomeController(ISampleRepository sampleRepository, IProducer producer)
-    {
-        _sampleRepository = sampleRepository;
-        _producer = producer;
-    }
-
     public ActionResult Index()
     {
-        return View(_sampleRepository.GetSamples());
-    }
-
-    public ActionResult Create()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public ActionResult Create(Sample sample)
-    {
-        _sampleRepository.Create(sample);
-        _producer.SendToSamples("SampleCreated");
-        return RedirectToAction("Index");
+        return RedirectToAction("Index", "Sample");
     }
 }
---
+++ SampleTracking/Controllers/SampleController.cs
@@ -0,0 +1,35 @@
+using MessageBroker.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SampleTracking.Models;
+
+namespace SampleTracking.Controllers;
+
+public class SampleController : Controller
+{
+    private readonly ISampleRepository _sampleRepository;
+    private readonly IProducer _producer;
+
+    public SampleController(ISampleRepository sampleRepository, IProducer producer)
+    {
+        _sampleRepository = sampleRepository;
+        _producer = producer;
+    }
+
+    public ActionResult Index()
+    {
+        return View(_sampleRepository.GetSamples());
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Sample sample)
+    {
+        _sampleRepository.Create(sample);
+        _producer.SendToSamples("SampleCreated");
+        return RedirectToAction("Index");
+    }
+}
